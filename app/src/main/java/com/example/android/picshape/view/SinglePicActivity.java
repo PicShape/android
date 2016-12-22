b672bdf368a7003709bbce168adae93d99257b97
Emerik <emerikbedouin@MacBook-Pro-de-Emerik.local>
2016-12-22 21:53:16 +0100

    [MOD] Modification of list item layout for the gallery

--- app/src/main/java/com/example/android/picshape/view/SinglePicActivity.java
+++ app/src/main/java/com/example/android/picshape/view/SinglePicActivity.java
@@ -2,14 +2,27 @@ package com.example.android.picshape.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.android.picshape.R;
 
 public class SinglePicActivity extends AppCompatActivity {
 
+    //View
+    private ImageView mImage;
+    private TextView mUsername, mPicTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_pic);
+    }
+
+    /**
+     * This function inialize the component
+     */
+    public void initComp(){
+
     }
 }
